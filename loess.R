509d6088435a072a39879162ceeab5e41c550662
clas-adm-demetris <56597686+athienit@users.noreply.github.com>
2021-09-17 08:30:38 -0400

    Update loess.R

--- loess.R
+++ loess.R
@@ -6,7 +6,7 @@ toluca.reg=lm(workhrs~lotsize,data=toluca)
 summary(toluca.reg)
 abline(toluca.reg,col="darkgreen")
 
-source("http://www.stat.ufl.edu/~athienit/check.R")
+source("https://raw.githubusercontent.com/athienit/STA4210material/main/check.R")
 check(toluca.reg,tests=TRUE)
 
 mycount=seq(20,120,by=1)
@@ -21,5 +21,5 @@ lines(mycount,predl$fit+1.96*predl$se.fit, lty=2, col="blue", lwd=1)
 polygon(c(mycount,rev(mycount)),c(predl$fit+1.96*predl$se.fit,rev(predl$fit-1.96*predl$se.fit)),col="#00009933",border="NA")
 
 abline(toluca.reg,col="darkgreen")
-legend("topleft",legend=c(expression(paste("Loess  ",alpha,"=0.5")),"95% CB","SLR"),
-       col=c("darkred","blue","darkgreen"),lty=c(5,2,1),bg="gray90")
+legend("bottomright",legend=c(expression(paste("Loess  ",alpha,"=0.5")),"95% CB","SLR"),
+       col=c("darkred","blue","darkgreen"),lty=c(5,2,1),bty="n")
