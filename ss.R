35899a1017f631849cc6c0699ecaa949aa98ee22
clas-adm-demetris <56597686+athienit@users.noreply.github.com>
2021-09-27 19:46:51 -0400

    Update ss.R

--- ss.R
+++ ss.R
@@ -17,3 +17,12 @@ summary.ss(reg)
 predict.ss(reg,x=0.3)
 
 plot.ss(reg) #plots confidence band
+
+# How about a cubic multiple regression, as "good" as SS?
+# b0+b1*x+b2*x^2+b3*x^3
+creg=lm(y~x+I(x^2)+I(x^3))
+summary(creg)
+plot(x,y,col=2)
+points(x,predict(creg),type="l", lty=2) #plot cubic regression
+lines(reg,lwd=2) #plot ss
+legend("bottomright", legend = c("ss", "cubic lm"), lty = 1:2,lwd=2:1, bty = "n")
