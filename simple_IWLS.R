3452ca9423c99dfc29c2cc256bbe9539d24748b0
clas-adm-demetris <56597686+athienit@users.noreply.github.com>
2021-11-19 21:03:59 -0500

    Update simple_IWLS.R

--- simple_IWLS.R
+++ simple_IWLS.R
@@ -20,13 +20,25 @@ summary(reg.iwls)
 glm.reg=glm(pressure~age,data=heart,method="glm.fit")
 summary(reg.iwls)
 
+#####################################
 ##### ONE ITERATION MANUALLY of IRLS
-#(1) obtain residuals
+#####################################
+##(1) obtain residuals from OLS
 e=residuals(reg)
-#(2) obtain robust residuals
+##(2) obtain robust residuals
 u=e/mad(e)
-#(3) use huber weight function
+##(3) use huber weight function on residuals
 w=psi.huber(u)
-#(4) fit WLS
+##(4) fit WLS
+#(a) automatically, obtain residuals from this model and repeat
 reg_v2=lm(formula(reg),weights=w,data=heart)
-#(5) obtain residuals from this model and repeat
+
+#(b) manually
+X=model.matrix(reg)
+y=heart$pressure
+W=diag(w)
+H=X%*%solve(t(X)%*%W%*%X)%*%t(X)%*%W
+ev2=(diag(length(y))-H)%*%y #recall e=(I-H)y
+
+#lets check if (a)=(b) to 4 decimal places
+round(ev2,4)==round(resid(reg_v2),4)
