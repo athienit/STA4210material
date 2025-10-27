7eadfc3b9fe4d95c8bf15d36f72326110c0fff5b
Demetris Athienitis <56597686+athienit@users.noreply.github.com>
2025-10-27 13:08:23 -0400

    Update linthurst.R

--- linthurst.R
+++ linthurst.R
@@ -10,31 +10,39 @@ print(linthurst)
 # regression model
 linthurst.model <- lm(biomass ~ salinity + pH + K + Na + Zn, data=linthurst)
 summary(linthurst.model)
-anova(linthurst.model)
 
 # correlation matrix
 round(cor(linthurst[c("biomass","salinity","pH","K","Na","Zn")]),3)  
 # correlation between (Zn and pH) and (Na and K)
 
-# Remove K
-modu1<- update(linthurst.model, . ~ . -K)
+# Remove Na
+modu1<- update(linthurst.model, . ~ . -Na)
 summary(modu1)
 
-# Remove Zn
-modu2<- update(linthurst.model, . ~ . -K-Zn)
+# Remove Na and salinity
+modu2<- update(linthurst.model, . ~ . -Na-salinity)
 summary(modu2)
 
-# Remove salinity
-modu3<- update(linthurst.model, . ~ . -K-Zn-salinity)
+# Remove Na and salinity and Zn
+modu3<- update(linthurst.model, . ~ . -Na-salinity-Zn)
 summary(modu3)
+AIC(modu3)
 
-# Remove Na
-modu4<- update(linthurst.model, . ~ . -K-Zn-salinity-Na)
+# Remove K too?
+modu4<- update(linthurst.model, . ~ . -Na-salinity-Zn-K)
 summary(modu4)
+
+# Final model with Na instead of K?
+modu5<-update(modu3, .~.-K+Na)
+summary(modu5)
+AIC(modu5)
+
+# simultaneous full vs reduced model
+anova(modu5,linthurst.model)
 
 # Fit biomass for pH=4.15 AND Na=10000. and create a P.I.
 newdata=data.frame(pH=4.15,Na=10000)
-predict(modu3, newdata, interval="prediction",level=0.95)
+predict(modu5, newdata, interval="prediction",level=0.95)
 
 # Next one should check the assumptions
 source("http://www.stat.ufl.edu/~athienit/check.R")
